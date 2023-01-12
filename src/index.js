4b696ca7db02a8ceff90c10c19d358d0e98c46c7
= <=>
2023-01-12 14:40:41 -0800

    Commit Pearce's changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { render } from 'react-dom';
 import CandleStickChartWithDarkTheme from './CandleStickChartWithDarkTheme';
 import Row from "./Row";
@@ -34,56 +34,87 @@ const ChartComponent = () => {
   const sigmaCols = useElementColumns(config.source);
   const sigmaData = useElementData(config.source);
 
+  const data = useMemo(() => {
+	const result = [];
+
   console.log("SIGMA DATA", sigmaData);
 
-  // create storage for column IDs 
-  const columnIds = {};
+	if (Object.keys(sigmaData).length) {
 
-  // create storage for transformed data
-  const newData = {}; 
-  const output = [];
+		const entries = Object.entries(sigmaData);
 
-  // iterate through the sigma columns and populate the column IDs object with key-value pairs 
-  // matching the hashed column alias to the plain text column name in Sigma
-  Object.keys(sigmaCols).map(key => columnIds[sigmaCols[key].id] = sigmaCols[key].name);
+		for (let i = 0; i < entries[0][1].length; i++) {
+			const row = {};
 
-  // iterate through an array of the sigma data column alias names 
-  Object.keys(sigmaData).map(key => {
-	// create a new property of newData named after the alias' user-facing name, and asssign it to that column's array of values
-	newData[columnIds[key]] = sigmaData[key];
-	// delete the original property after the new one has been created
-	// delete sigmaData[key];
-  })
+			for (const [columnId, values] of entries) {
+				const columnName = sigmaCols[columnId].name.toLowerCase();
 
-  console.log("NEW DATA", newData);
+				let value = values[i];
+				if (columnName === 'date') {
+					value = new Date(values[i]);
+				}
+
+				row[columnName] = value;
+			}
+
+			result.push(row);
+		}
+	}
+
+	return result;
+  }, [sigmaCols, sigmaData])
+
+//   console.log("SIGMA COLS", sigmaCols);
+//   console.log("SIGMA DATA", sigmaData);
+
+//   // create storage for column IDs 
+//   const columnIds = {};
+
+//   // create storage for transformed data
+//   const newData = {}; 
+//   const output = [];
+
+//   // iterate through the sigma columns and populate the column IDs object with key-value pairs 
+//   // matching the hashed column alias to the plain text column name in Sigma
+//   Object.keys(sigmaCols).map(key => columnIds[sigmaCols[key].id] = sigmaCols[key].name);
+
+//   // iterate through an array of the sigma data column alias names 
+//   Object.keys(sigmaData).map(key => {
+// 	// create a new property of newData named after the alias' user-facing name, and asssign it to that column's array of values
+// 	newData[columnIds[key]] = sigmaData[key];
+// 	// delete the original property after the new one has been created
+// 	// delete sigmaData[key];
+//   })
+
+//   console.log("NEW DATA", newData);
 
 
-  if(newData.Ticker) {
-	for (let i = 0; i < newData.Ticker.length; i++) {
-		output.push(new Row(
-			newData.Ticker[i],
-			new Date(newData.Date[i]).toISOString(),
-			newData.High[i],
-			newData.Low[i],
-			newData.Open[i],
-			newData.Close[i],
-			newData.Volume[i]
-		))
-	};
-	console.log("OUTPUT",JSON.stringify(output));
-  }
+//   if(newData.Ticker) {
+// 	for (let i = 0; i < newData.Ticker.length; i++) {
+// 		output.push(new Row(
+// 			newData.Ticker[i],
+// 			new Date(newData.Date[i]).toISOString(),
+// 			newData.High[i],
+// 			newData.Low[i],
+// 			newData.Open[i],
+// 			newData.Close[i],
+// 			newData.Volume[i]
+// 		))
+// 	};
+// 	console.log("OUTPUT",JSON.stringify(output));
+//   }
 
-  const [data, setData] = React.useState(null);
+//   const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-	console.log("Using effect");
-	getData().then(data => {
-	  console.log("DATA", JSON.stringify(data));
-	  setData(data);
-	})
-  }, []);
+//   React.useEffect(() => {
+// 	console.log("Using effect");
+// 	getData().then(data => {
+// 	  console.log("DATA", JSON.stringify(data));
+// 	  setData(data);
+// 	})
+//   }, []);
 
-  if(!data) {
+  if(!data || !data.length) {
     return (<div>Loading...</div>)
   }
 
