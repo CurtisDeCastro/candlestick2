280b94aa05d51fc34d0fa29381786265cd6a5cde
= <=>
2023-01-17 17:30:30 -0800

    Remove unnecessary files

--- src/Row.js
+++ src/Row.js
@@ -11,9 +11,3 @@ class Row {
 }
 
 export default Row;
-
-const sampleRows = [
-	{"Ticker":"AAPL","date":"1980-12-07T08:00:00.000Z","high":29,"low":29,"open":29,"close":29,"volume":2093900},
-	{"Ticker":"AAPL","date":"1980-12-14T08:00:00.000Z","high":28,"low":25,"open":27,"close":28,"volume":2188100},
-  ];
-
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useMemo } from 'react';
 import { render } from 'react-dom';
 import CandleStickChartWithDarkTheme from './CandleStickChartWithDarkTheme';
-import Row from "./Row";
 import Background from "./Background";
-
-// import sigma hooks and other utilities
-import { getData } from "./utils";
 
 import {
 	client,
@@ -17,11 +13,11 @@ import {
 // config source columns
 client.config.configureEditorPanel([
 	{ name: "source", type: "element" },
-	{ name: "Ticker", type: "column", source: "source", allowMultiple: false },
+	{ name: "Ticker", type: "column", source: "source", allowMultiple: false, allowedTypes: ['text'] },
 	{ name: "Date", type: "column", source: "source", allowMultiple: false, allowedTypes: ['datetime'] },
-	{ name: "High", type: "column", source: "source", allowMultiple: false },
-	{ name: "Low", type: "column", source: "source", allowMultiple: false },
-	{ name: "Open", type: "column", source: "source", allowMultiple: false },
+	{ name: "High", type: "column", source: "source", allowMultiple: false, allowedTypes: ['number', 'integer'] },
+	{ name: "Low", type: "column", source: "source", allowMultiple: false, allowedTypes: ['number', 'integer'] },
+	{ name: "Open", type: "column", source: "source", allowMultiple: false, allowedTypes: ['number', 'integer'] },
 	{ name: "Close", type: "column", source: "source", allowMultiple: false, allowedTypes: ['number', 'integer'] },
 	{ name: "Volume", type: "column", source: "source", allowMultiple: false, allowedTypes: ['number', 'integer'] },
 ]);
@@ -36,83 +32,25 @@ const ChartComponent = () => {
 
   const data = useMemo(() => {
 	const result = [];
-
-  console.log("SIGMA DATA", sigmaData);
-
 	if (Object.keys(sigmaData).length) {
-
 		const entries = Object.entries(sigmaData);
-
 		for (let i = 0; i < entries[0][1].length; i++) {
 			const row = {};
-
 			for (const [columnId, values] of entries) {
-				const columnName = sigmaCols[columnId].name.toLowerCase();
-
-				let value = values[i];
-				if (columnName === 'date') {
-					value = new Date(values[i]);
+				if (sigmaCols[columnId].name) {
+					const columnName = sigmaCols[columnId].name.toLowerCase();
+					let value = values[i];
+					if (columnName === 'date') {
+						value = new Date(values[i]);
+					}
+					row[columnName] = value;
 				}
-
-				row[columnName] = value;
 			}
-
 			result.push(row);
 		}
 	}
-
 	return result;
   }, [sigmaCols, sigmaData])
-
-//   console.log("SIGMA COLS", sigmaCols);
-//   console.log("SIGMA DATA", sigmaData);
-
-//   // create storage for column IDs 
-//   const columnIds = {};
-
-//   // create storage for transformed data
-//   const newData = {}; 
-//   const output = [];
-
-//   // iterate through the sigma columns and populate the column IDs object with key-value pairs 
-//   // matching the hashed column alias to the plain text column name in Sigma
-//   Object.keys(sigmaCols).map(key => columnIds[sigmaCols[key].id] = sigmaCols[key].name);
-
-//   // iterate through an array of the sigma data column alias names 
-//   Object.keys(sigmaData).map(key => {
-// 	// create a new property of newData named after the alias' user-facing name, and asssign it to that column's array of values
-// 	newData[columnIds[key]] = sigmaData[key];
-// 	// delete the original property after the new one has been created
-// 	// delete sigmaData[key];
-//   })
-
-//   console.log("NEW DATA", newData);
-
-
-//   if(newData.Ticker) {
-// 	for (let i = 0; i < newData.Ticker.length; i++) {
-// 		output.push(new Row(
-// 			newData.Ticker[i],
-// 			new Date(newData.Date[i]).toISOString(),
-// 			newData.High[i],
-// 			newData.Low[i],
-// 			newData.Open[i],
-// 			newData.Close[i],
-// 			newData.Volume[i]
-// 		))
-// 	};
-// 	console.log("OUTPUT",JSON.stringify(output));
-//   }
-
-//   const [data, setData] = React.useState(null);
-
-//   React.useEffect(() => {
-// 	console.log("Using effect");
-// 	getData().then(data => {
-// 	  console.log("DATA", JSON.stringify(data));
-// 	  setData(data);
-// 	})
-//   }, []);
 
   if(!data || !data.length) {
     return (<div>Loading...</div>)
